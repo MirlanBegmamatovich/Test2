dc301e72a6b56e0f9f152fa5ca7799aa0515190f
User <fastrainy@gmail.com>
2020-05-17 00:22:14 +0600

    Kyrgyzstan

---
+++ src/com/company/LessonThree/HomeWork/Kyrgyzstan.java
@@ -0,0 +1,67 @@
+package com.company.LessonThree.HomeWork;
+
+public class Kyrgyzstan {
+    int id;
+    String name;
+    double balanse;
+
+    public static void main(String[] args){
+        Kyrgyzstan aA = new Kyrgyzstan();
+        aA.id = 1;
+        aA.name = "Чуй";
+        aA.balanse = 1638973;
+        System.out.println(" ID области " + aA.id);
+        System.out.println("Наименование области " + aA.name);
+        System.out.println("Население " + aA.balanse);
+
+        Kyrgyzstan aB = new Kyrgyzstan();
+         aB.id = 2;
+         aB.name = "Ош";
+         aB.balanse = 1362359;
+        System.out.println(" ID области " + aB.id);
+        System.out.println("Наименование области " + aB.name);
+        System.out.println("Население " + aB.balanse);
+
+        Kyrgyzstan aC = new Kyrgyzstan();
+        aC.id = 3;
+        aC.name = "Жалалабад";
+        aC.balanse = 1009889;
+        System.out.println("ID области " + aC.id);
+        System.out.println("Наименование области " + aC.name);
+        System.out.println("Население " + aC.balanse);
+
+        Kyrgyzstan aD = new Kyrgyzstan();
+        aD.id = 4;
+        aD.name = "Баткен";
+        aD.balanse = 428636;
+        System.out.println("ID области " + aD.id);
+        System.out.println("Наименование области " + aD.name);
+        System.out.println("Население " + aD.balanse);
+
+        Kyrgyzstan aE = new Kyrgyzstan();
+        aE.id = 5;
+        aE.name = "Талас";
+        aE.balanse = 226779;
+        System.out.println("ID области " + aE.id);
+        System.out.println("Наименование области " + aE.name);
+        System.out.println("Население " + aE.balanse);
+
+        Kyrgyzstan aF = new Kyrgyzstan();
+        aF.id = 5;
+        aF.name = "Нарын";
+        aF.balanse = 257768;
+        System.out.println("ID области " + aF.id);
+        System.out.println("Наименование области " + aF.name);
+        System.out.println("Население " + aF.balanse);
+
+        Kyrgyzstan aG = new Kyrgyzstan();
+        aG.id = 6;
+        aG.name = "Ысык-Кол";
+        aG.balanse = 438389;
+        System.out.println("ID области " + aG.id);
+        System.out.println("Наименование области " + aG.name);
+        System.out.println("Население " + aG.balanse);
+
+
+    }
+}
